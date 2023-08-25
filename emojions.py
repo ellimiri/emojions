bddb12cef982aac50ceb6e0b6d578d062d97d33d
Miriam Elliott Haynes <ellimiri@outlook.com>
2023-08-25 22:48:35 +1000

    we do a little cropping

--- emojions.py
+++ emojions.py
@@ -1,4 +1,4 @@
-from cv2 import flip, imshow, rectangle, waitKey, namedWindow, VideoCapture, destroyWindow
+from cv2 import flip, imshow, rectangle, waitKey, namedWindow, VideoCapture, destroyWindow, cvtColor, COLOR_BGR2GRAY
 from utils.face_crop import FaceCropper
 
 namedWindow("test")
@@ -8,22 +8,27 @@ face_cropper: FaceCropper = FaceCropper()
 
 if vc.isOpened(): # try to get the first frame
     rval, frame = vc.read()
-    frame = flip(frame, 1)
+    image = flip(frame, 1)
 else:
     rval = False
 
 while rval:
-    imshow("preview", frame) # show current frame
+    imshow("preview", image) # show current frame
 
     # read and flip frame from the video capture
     rval, frame = vc.read()
-    frame = flip(frame, 1)
+    image = flip(frame, 1)
+    gray_image = cvtColor(image, COLOR_BGR2GRAY)
 
     # face is a tuple with x, y, width and height; extract
-    face_detected, face = face_cropper.get_face(frame)
+    face_detected, face = face_cropper.get_face(gray_image)
     if face_detected:
-        x, y, w, h = face
-        rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 4)
+        # x, y, w, h = face
+        # rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 4)
+
+        image = face_cropper.get_face_img(gray_image, face)
+    else:
+        image = frame
 
     key = waitKey(20)
     if key == 27: # exit on ESC
--- utils/face_crop.py
+++ utils/face_crop.py
@@ -1,17 +1,30 @@
-from cv2 import CascadeClassifier, data, cvtColor, COLOR_BGR2GRAY, Rect
+from cv2 import CascadeClassifier, data, resize
+import numpy as np
 
 class FaceCropper:
 
     def __init__(self):
+        # parameters for face detection
         self.face_classifier: CascadeClassifier = \
             CascadeClassifier(data.haarcascades + "haarcascade_frontalface_default.xml")
+        self.scale_factor = 1.1
+        self.min_neighbours = 5
+        self.min_size = (48, 48)
+
+        # parameters for image cropping
+        self.face_img_size = 220
         
     def get_face(self, frame):
-        """Returns (bool, Rect): boolean value for whether any face was found, and bounding Rect
-        for the largest face found."""
-        gray_image = cvtColor(frame, COLOR_BGR2GRAY)
+        """
+        Returns (bool, Rect): boolean value for whether any face was found, and bounding Rect
+        for the largest face found.
+        Please ensure that the frame is in greyscale before sending through!
+        """
         faces = self.face_classifier.detectMultiScale(
-            gray_image, scaleFactor=1.1, minNeighbors=5, minSize=(40, 40))
+            frame, 
+            scaleFactor=self.scale_factor, 
+            minNeighbors=self.min_neighbours, 
+            minSize=self.min_size)
         
         # Get the largest face rect and return only that
         biggest_face = None
@@ -30,4 +43,9 @@ class FaceCropper:
         return face_detected, biggest_face
 
     def get_face_img(self, frame, face_rect):
-        pass
+        """Please ensure that the frame is in greyscale before sending through!"""
+        x, y, w, h = face_rect
+        frame_crop = frame[y:y + h, x:x + w]
+        # Must crop to 48x48 to match the emotion recognition dataset
+        face_img = np.copy(resize(frame_crop, (48, 48)))
+        return face_img
